a529bfbc42fa020b23d8f9b7b414554cd69ba0aa
Gilbert <gilbert.j5@outlook.com>
2018-01-23 20:28:25 -0500

    add fetchWeather action

--- dev/scripts/actions/index.js
+++ dev/scripts/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '493025e2689c9bafc99f7f5d2cd909d7';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},CA`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
